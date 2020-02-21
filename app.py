df656ae1d8206a23e5dcf4cc84ec4fb4c7f22a40
will <818@holbertonschool.com>
2020-02-21 03:36:11 +0000

    [BUGFIX] Fix get and post request forwarding

--- app.py
+++ app.py
@@ -8,17 +8,19 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def home():
     return render_template("index.html")
 
+
 @app.route("/api/<path:req>", methods=['POST', 'GET'])
 def api_req(req):
-    return "Yes"
     base_path = 'https://intranet.hbtn.io/'
+    #base_path = 'https://jsonplaceholder.typicode.com/'
     if request.method == 'GET':
-    	res = requests.get(base_path + req).text
-        return (res, res.status_code)
+        params = request.args
+        res = requests.get(base_path + req, params=request.args)
+        return (res.text, res.status_code)
     if request.method == 'POST':
         data = request.form.to_dict()
-        res = requests.post(base_path + req, data=data).text
-        return (res, res.status_code)
+        res = requests.post(base_path + req, data=data)
+        return (res.text, res.status_code)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
