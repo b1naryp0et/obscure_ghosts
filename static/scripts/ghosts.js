fec429388013fe84a203588e63eaab99d527dc52
will <818@holbertonschool.com>
2020-02-21 14:20:15 +0000

    [RESTRUCTURE] Move Johns auth getting into a function

--- static/scripts/ghosts.js
+++ static/scripts/ghosts.js
@@ -1,36 +1,41 @@
-var pwd = undefined;
+function getAuth () {
+  // Get the auth key from the given data.
+  let pwd = undefined;
+  let email = undefined;
+  let apiKey = undefined;
+  const url = './api/users/auth_token.json';
+  
+  pwd = $('#passwd').val();
+  email = $('#email').val();
+  apiKey = $('#apiKey').val();
+
+  if (pwd === '' || email === '' || apiKey === '') {
+    alert('One or more fields empty');
+    return
+  }
+
+  let x = {};
+  x.password = pwd;
+  x.api_key = apiKey;
+  x.email = email;
+  x.scope = 'checker';
+
+  $.post(url,x, function (data, status) {
+    auth = data.auth_token;
+  },
+  'json');
+}
+
 var auth = undefined;
-var email = undefined;
-var apiKey = undefined;
-const url = './api/users/auth_token.json';
-
-
 $(document).ready (function () {
 
-  $('#passwd').change(function () {
-    pwd = $('#passwd').val();
+  //--- Get the auth key on button click or pressing enter ---vv
+  $('#submit').click(getAuth);
+  $('#passwd, #email, #apiKey').keyup(function (e) {
+    if (e.keyCode === 13) {
+      getAuth();
+    }
   });
+  //----------------------------------------------------------^^
 
-  $('#email').change(function () {
-    email = $('#email').val();
-  });
-
-  $('#apiKey').change(function () {
-    apiKey = $('#apiKey').val();
-  });
-
-  $('#submit').click(function () {
-    let x = {};
-    x.password = pwd;
-    x.api_key = apiKey;
-    x.email = email;
-    x.scope = 'checker';
-
-    $.post(url,x, function (data, status) {
-      alert(data);
-      alert(status);
-      auth = data.auth_token;
-    },
-    'json');
-  });
 });
