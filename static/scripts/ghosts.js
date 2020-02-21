cc7ef09c4104c3583917e9528fa6d6ef1d3de93c
jfkquality <jfkquality@gmail.com>
2020-02-21 03:42:50 +0000

    ghost.js init commit.

--- static/scripts/ghosts.js
+++ static/scripts/ghosts.js
@@ -1,6 +1,32 @@
-var x = undefined;
+var pwd = undefined;
+var auth = undefined;
+var email = undefined;
+var apiKey = undefined;
+const url = 'https://intranet.hbtn.io/users/auth_token.json';
+
+
 $(document).ready (function () {
-    $.get('./api/john', function(data, status){
-	alert("Data: " + data + "\nStatus: " + status);
-    });
+
+  $('#name').click(function () {
+    let name = $('#name').val();
+  });
+
+  $('#email').click(function () {
+    let email = $('#email').val();
+  });
+
+  $('#apiKey').click(function () {
+    let apiKey = ('#apiKey').val();
+  });
+
+  $('input').click(function () {
+    let x = {};
+    x.password = pwd;
+    x.api_key = apiKey;
+    x.email = email;
+
+    $.post(url,x, function (data, status), 'json') {
+      auth = data.auth_token;
+    }
+  });
 });
