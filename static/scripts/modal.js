84b99e6a55d956a07367e651bf4ef30e9e8638f1
jfkquality <jfkquality@gmail.com>
2020-02-21 22:53:01 -0500

    Add strikethrough to modal text.

--- static/scripts/modal.js
+++ static/scripts/modal.js
@@ -1,35 +1,41 @@
 $(window).ready (function () {
-    var modal = document.getElementById("myModal");
+  var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-    modal.style.display = "block";
+  modal.style.display = "block";
 
-    // When the user clicks on the button, open the modal
-    // btn.onclick = function() {
-    // 	modal.style.display = "block";
-    // }
+  // When the user clicks on the button, open the modal
+  // btn.onclick = function() {
+  // 	modal.style.display = "block";
+  // }
 
-    // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    // 	modal.style.display = "none";
-    // }
+  // when the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  // 	modal.style.display = "none";
+  // }
 
-    //make sure you have lower case "o"
+  //make sure you have lower case "o"
+  let sound = document.getElementById("opening");
+  sound.currentTime = 0;
+  sound.loop = true; //if you want it to restart playing automatically when it ends
+  sound.play();
 
-    var that = this;
-    setTimeout(function() {
-	modal.style.dispaly = "none";
-    }, 5000);
+  var that = this;
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 5000);
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-	if (event.target == modal) {
-	    modal.style.display = "none";
-	}
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
+  }
+  // var player = document.getElementById("opening");
+  // player.play;
 });
